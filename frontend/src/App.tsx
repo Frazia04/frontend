4b374365fdec81efb1dc896cd2fefed71aa5bd16
Frazia Farheen <tek79puz@rptu.de>
2025-02-26 01:35:18 +0100

    Added Newpage route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router";
 
 import Layout from "@/app/dashboard/page";
 import Testpage from "@/app/testpage/page";
+import Newpage from "@/app/newpage/page";
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <Route path="/" element={<Layout />}>
                 <Route index element={<Testpage />} />
             </Route>
+            <Route path="/newpage" element={<Newpage />}></Route>
         </Routes>
     );
 }
